4e9f37a9533fc46d22e8da14beb793376c382ff2
Krishna Pranathi Naidu <krishna.naidu@stud.hslu.ch>
2024-10-17 04:00:15 +0200

    Initial config part 2 of like, idek

--- Website/views.py
+++ Website/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 #this is a blueprint: which means that it has a bunch of URLs defined in it.
-#A blueprint is a template for generating a "section" of a web application. You can think of it as a mold
+#A blueprint is a templates for generating a "section" of a web application. You can think of it as a mold
 
 @views.route('/')
 def home(): #function runs when you go to the URL above
-    return "<h1> TEST <h1>"
+    return render_template("home.html")
